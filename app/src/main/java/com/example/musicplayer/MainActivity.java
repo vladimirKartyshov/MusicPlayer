65936d0fb424d4bc8e8484996a1f7e1b2b1f3118
vladimirKartyshov <vovan198080@inbox.ru>
2019-10-31 15:17:30 +0300

    seek bar

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,14 +1,84 @@
 package com.example.musicplayer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    ImageView playPauseIcon;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playPauseIcon = findViewById(R.id.playIconImageView4);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(mediaPlayer.getDuration());// максимум = длит-ти композиции
+               //чтобы управлять ползунком
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {// управляем seek bar
+            @Override
+            public void run() {
+                //устанавливаем seekbar в текущую позицию плеера
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 1000);//запускается сразу с переодичностью 1сек
+    }
+
+
+    public void previous(View view) {
+        seekBar.setProgress(0);//устанавливаем в начало
+        mediaPlayer.seekTo(0);
+        mediaPlayer.pause();
+        playPauseIcon.setImageResource(R.drawable.ic_play_arrow_orange_24dp);
+    }
+
+    public void next(View view) {
+        seekBar.setProgress(mediaPlayer.getDuration());
+        mediaPlayer.seekTo(mediaPlayer.getDuration());
+        mediaPlayer.pause();
+        playPauseIcon.setImageResource(R.drawable.ic_play_arrow_orange_24dp);
+    }
+
+    public void play(View view) {
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+            playPauseIcon.setImageResource(R.drawable.ic_play_arrow_orange_24dp);
+        }else {
+            mediaPlayer.start();
+            playPauseIcon.setImageResource(R.drawable.ic_pause_orange_24dp);
+        }
     }
 }
